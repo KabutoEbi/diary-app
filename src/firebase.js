45b6b50c06212edaa9dd5ab4c35b9672da2f1144
KabutoEbi <narenohatehe@gmail.com>
2025-07-19 15:47:10 +0900

    move firebase secres to .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD92SXvuZuJ9y9VVEj_p06uWGtYjBratUM",
-  authDomain: "diary-app-2ed15.firebaseapp.com",
-  projectId: "diary-app-2ed15",
-  storageBucket: "diary-app-2ed15.appspot.com",
-  messagingSenderId: "834705668218",
-  appId: "1:834705668218:web:ff52e564c8a18583dd63c3",
-  measurementId: "G-MQCMB5FQDV"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
